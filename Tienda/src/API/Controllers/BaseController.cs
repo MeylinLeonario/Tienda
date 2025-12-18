fcb0c610664104d5e2cc9f85493467aa8240088a
Meylin Leonario <167142123+MeylinLeonario@users.noreply.github.com>
2025-12-18 00:44:10 -0300

    Revert "Fix/reorganization"

--- Tienda/src/API/Controllers/BaseController.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Tienda.src.api.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BaseController : ControllerBase;
-}
